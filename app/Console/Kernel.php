e8108b2a66891b4d2ec814cfe2fe1b17195c8e5a
Sam Abhishek <abhishek.bandari@paulstechnologies.com>
2025-10-09 10:12:24 +0400

    Deploy latest changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,22 +9,24 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // Run Meta lead import every hour
+        $schedule->command('leads:import-meta --company=1 --limit=50')
+         ->everyFifteenMinutes()
+         ->withoutOverlapping()
+         ->sendOutputTo(storage_path('logs/meta_cron.log'));
+
+    }
+
+    /**
      * Register the commands for the application.
      */
     protected function commands(): void
     {
+        // automatically loads all artisan commands in app/Console/Commands/
         $this->load(__DIR__.'/Commands');
     }
-
-    /**
-     * Define the application's command schedule.
-     */
-    // app/Console/Kernel.php
-    protected function schedule(\Illuminate\Console\Scheduling\Schedule $schedule): void
-        {
-            $schedule->command('leads:import-meta')
-                ->everyFiveMinutes()
-                ->withoutOverlapping()
-                ->runInBackground();
-        }
 }
